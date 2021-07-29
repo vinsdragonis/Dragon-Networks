2ca309a227923c46bd3a9c0444248df3f3509dc5
JahnaviSatishShanbhag <jahnavisatish.cs19@bmsce.ac.in>
2021-07-29 15:35:44 +0530

    addded authentication and chamged the folder structure commit

--- app.js
+++ app.js
@@ -1,87 +1,56 @@
 //jshint esversion:6
 
 const express = require("express");
+const app = express();
 const ejs = require("ejs");
-const bodyParser = require("body-parser");
 const _ = require('lodash');
 const mongoose = require("mongoose");
-
-const app = express();
+const cookieParser=require('cookie-parser');
+const {Post}=require('./models/post');
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true})
+// .then(() => console.log('Connected to mongodb'))
+// .catch(() => console.log('Could not connect to mongodb'));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Connected to mongodb'))
+.catch(() => console.log('Could not connect to mongodb'));
 
-const postSchema = {
-  postTitle: String,
-  postAuthor: String,
-  postContent: String
-};
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(__dirname+'/public'));
 
-const Post = mongoose.model("Post", postSchema);
+app.use('/auth',require('./routers/users'));
+app.use('/',require('./routers/home'));
+app.use('/compose',require('./routers/compose'));
+app.use('/about',require('./routers/about'));
+app.use('/contact',require('./routers/contact'));
 
-const defaultBlog = new Post ({
-  postTitle: "Welcome",
-  postAuthor: "Dragon Networks",
-  postContent: "Hey there! Click on the compose button above to write a blog, or checkout some of the recent blogs written by other people."
-});
-
-app.get("/", function(req, res) {
-  Post.find({}, function(err, posts){
-    res.render("home", {posts: posts});
-  });
-});
-
-app.post("/", function(req, res) {
-  if (req.body.home === "Compose") {
-    res.redirect("/compose");
-  } else if (req.body.home === "About") {
-    res.redirect("/about");
-  } else if (req.body.home === "Contact") {
-    res.redirect("/contact");
-  } else {
-    res.redirect("/");
-  }
-});
-
-app.get("/compose", function(req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res) {
-  const post = new Post ({
-    postTitle: req.body.postTitle,
-    postAuthor: req.body.postAuthor,
-    postContent: req.body.postContent
-  });
-
-  post.save(function(err){
-    if (!err){
-        res.redirect("/");
-    }
-  });
-});
-
-app.get("/about", function(req, res) {
-  res.render("about");
-});
-
-app.get("/contact", function(req, res) {
-  res.render("contact");
-});
+// const defaultBlog = new Post ({
+//   postTitle: "Welcome",
+//   postAuthor: "Dragon Networks",
+//   postContent: "Hey there! Click on the compose button above to write a blog, or checkout some of the recent blogs written by other people."
+// });
 
 app.get("/posts/:postId", function(req, res) {
   const reqPostId = req.params.postId;
 
   Post.findOne({_id: reqPostId}, function(err, post) {
-    res.render("post", {
-      title: post.postTitle,
-      author: post.postAuthor,
-      content: post.postContent
-    });
+    if (err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      res.render("post", {
+        title: post.postTitle,
+        author: post.postAuthor,
+        content: post.postContent
+      });
+    }
   });
 });
 
